4f9791f015868f90ebc241c9b416199e0f6ddcd7
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-06-24 16:19:17 +0900

    add: Exercise for tuples

---
+++ ex_10/ex_10.py
@@ -0,0 +1,35 @@
+fname = input('Enter File: ')
+if len(fname) < 1 : fname = 'clown.txt'
+hand = open(fname)
+
+di = dict()
+for lin in hand :
+    lin = lin.rstrip()
+    wds = lin.split()
+    # 현재 단어 딕셔너리에 저장
+    for w in wds :
+        di[w] = di.get(w,0) + 1
+
+# 가장 자주 등장한 단어 찾기
+# largest: 가장 자주 등장한 단어 횟수
+# theword: 가장 자주 등장한 단어
+largest = -1
+theword = None
+for k, v in di.items() :
+    if v > largest : 
+        largest = v
+        theword = k
+        
+print(theword, largest)
+
+tmp = list()
+for k, v in di.items() :
+    new_tuple = (v, k)
+    tmp.append(new_tuple)
+print('Flipped', tmp)
+
+tmp = sorted(tmp, reverse=True)
+print('Sorted', tmp)
+
+for v, k in tmp[:5] :
+    print(v, k)
